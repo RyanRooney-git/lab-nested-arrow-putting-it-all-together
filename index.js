3337686c705930e154134ca7613ba91f6ed77170
Ryan Rooney <ryan.rooney42@yahoo.com>
2026-03-10 03:08:17 +0000

    Nested arrow lab completed

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0; 
 
+  const passwordAttempt = (userPassword) => {
+    attemptCount++; 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    if (userPassword === userInfo.password) {
+      return "Login successful";
+    } else {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+
+  return passwordAttempt;
+}
 
 
 module.exports = {
